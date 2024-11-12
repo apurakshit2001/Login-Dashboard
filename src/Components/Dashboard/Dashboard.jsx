e4624a47f8db35d4a294988adc48e6c47ce1597a
apurakshit2001 <okritrimoj@gmail.com>
2024-11-12 21:15:19 +0530

    "Added useState and useEffect to Dashboard component to retrieve and display user data from session storage."

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Dashboard = () => {
-  return (
-    <div>
-      cgcnhg
-    </div>
-  )
-}
+  const [userData, setUserData] = useState({ email: '', password: '' });
 
-export default Dashboard
+  useEffect(() => {
+    const storedUserData = JSON.parse(sessionStorage.getItem('userData'));
+    if (storedUserData) {
+      setUserData(storedUserData);
+    }
+  }, []);
+
+  return (
+    <div className='dashboard' style={{ textAlign: 'center',color: 'white'}}>
+      <h1>User Email: {userData.email}</h1>
+      <h1>Password: {userData.password}</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
